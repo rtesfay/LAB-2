b292c51a285704323c8c37d6a5ab76f3d17c9fec
rtesfay <91496249+rtesfay@users.noreply.github.com>
2021-09-30 21:33:08 -0500

    Update TemperatureQ3.java

--- src/TemperatureQ3.java
+++ src/TemperatureQ3.java
@@ -1,14 +1,1 @@
-import java.util.Scanner;
 
-public class TemperatureQ3 {
-    public static void main(String[] args) {
-        Scanner keyboard =new Scanner(System.in);
-        System.out.println("Enter the year you were born");
-        int yearOfBorn;
-        yearOfBorn= keyboard.nextInt();
-        int currentYear=0;
-        int age;
-        age= (currentYear-yearOfBorn);
-        System.out.println("You were born in "+yearOfBorn+ " and will be "+age+ " this year");
-    }
-}
